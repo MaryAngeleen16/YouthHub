aca2e010b6bd3d7b7a891858624460c56947de15
armangracia <graciaarmansteven@gmail.com>
2024-02-15 10:07:11 +0800

    navigation bar update

--- frontend/src/Components/Layouts/Header.js
+++ frontend/src/Components/Layouts/Header.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -7,21 +7,14 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
 import { getUser, logout } from '../../utils/helpers';
 import './FH.css';
-import CoffeeIcon from '@mui/icons-material/Coffee';
-import SearchIcon from '@mui/icons-material/Search';
-import { styled, alpha } from '@mui/material/styles';
-import InputBase from '@mui/material/InputBase';
-
 import PregnancyPostPage from '../PregnancyPostPage';
+
 const Header = () => {
   const [anchorEl, setAnchorEl] = useState(null);
   const [anchorElUser, setAnchorElUser] = useState(null);
@@ -54,108 +47,92 @@ const Header = () => {
   const settings = ['Profile', 'Settings'];
 
   return (
-    <AppBar position="static" className="bg">
-      <Toolbar>
-        {/* <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
-          <MenuIcon />
-        </IconButton> */}
+    <div>
+      <AppBar position="static" className="bg">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} className="text-ye" style={{ textAlign: 'left' }} href="/">
+            Youth Empowerment
+          </Typography>
 
-        <Typography
-          variant="h6"
-          component="div"
-          sx={{ flexGrow: 1 }}
-          className="text-ye"
-          style={{ textAlign: 'left'}}
-          href="/"
-        >
-          Youth Empowerment
-        </Typography>
-
-
-        <Link to="/Pregnancy" style={{ textDecoration: 'none', color: 'inherit' }}>
-        <Button
-          color="inherit"
-          onClick={handleClick}
-          aria-controls="tutorials-menu"
-          aria-haspopup="true"
-        
-        >
-           
-          Pregnancy
-        </Button>
-        </Link>
-        {/* <Menu
-          id="tutorials-menu"
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-        >
-         
-          <MenuItem onClick={handleClose}>Tutorial 1</MenuItem>
-          <MenuItem onClick={handleClose}>Tutorial 2</MenuItem>
-          <MenuItem onClick={handleClose}>Tutorial 3</MenuItem>
-        </Menu> */}
-        <Button color="inherit">Sexual Education</Button>
-        <Box>
-          {userAuthenticated ? (
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu}>
-                {user.avatar ? (
-                  <Avatar src={user.avatar.url} alt={user.name} />
-                ) : null}
-              </IconButton>
-            </Tooltip>
-          ) : (
-            <Link to="/login" className="btn ml-4" id="login_btn">
-              <Button className='login-text'>Login</Button>
-            </Link>
-          )}
-          {/* Removed IconButton with Account Circle Icon */}
-          <Menu
-            sx={{ mt: '45px' }}
-            id="menu-appbar"
-            anchorEl={anchorElUser}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            open={Boolean(anchorElUser)}
-            onClose={handleCloseUserMenu}
-          >
-            {settings.map((setting, index) => (
-              <MenuItem key={index} onClick={handleCloseUserMenu}>
-                <Link to="/me" style={{ textDecoration: 'none', color: 'inherit' }}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </Link>
-              </MenuItem>
-            ))}
-
-            {user && user.role === 'admin' && (
-              <MenuItem>
-                <Link to="/dashboard" style={{ textDecoration: 'none', color: 'inherit' }}>
-                  <Typography textAlign="center">Dashboard</Typography>
-                </Link>
-              </MenuItem>
+          {/* <Link to="/Pregnancy" style={{ textDecoration: 'none', color: 'inherit' }}>
+            <Button color="inherit" onClick={handleClick} aria-controls="tutorials-menu" aria-haspopup="true">
+              Pregnancy
+            </Button>
+          </Link> */}
+          {/* <Button color="inherit">Sexual Education</Button> */}
+          <Box>
+            {userAuthenticated ? (
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu}>
+                  {user.avatar ? (
+                    <Avatar src={user.avatar.url} alt={user.name} />
+                  ) : null}
+                </IconButton>
+              </Tooltip>
+            ) : (
+              <Link to="/login" className="btn ml-4" id="login_btn">
+                <Button className='login-text'>Login</Button>
+              </Link>
             )}
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting, index) => (
+                <MenuItem key={index} onClick={handleCloseUserMenu}>
+                  <Link to="/me" style={{ textDecoration: 'none', color: 'inherit' }}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </Link>
+                </MenuItem>
+              ))}
 
-            <MenuItem onClick={handleLogout}>
-              <Typography textAlign="center" color="red">Logout</Typography>
-            </MenuItem>
-          </Menu>
-        </Box>
-      </Toolbar>
-    </AppBar>
+              {user && user.role === 'admin' && (
+                <MenuItem>
+                  <Link to="/dashboard" style={{ textDecoration: 'none', color: 'inherit' }}>
+                    <Typography textAlign="center">Dashboard</Typography>
+                  </Link>
+                </MenuItem>
+              )}
+
+              <MenuItem onClick={handleLogout}>
+                <Typography textAlign="center" color="red">Logout</Typography>
+              </MenuItem>
+            </Menu>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item"><b>
+            <a className="nav-link" href="/">Home</a>
+          </b></li>
+          <li className="nav-item"><b>
+            <a className="nav-link" href="/collections">Trending Posts</a>
+          </b></li>
+          <li className="nav-item"><b>
+            <a className="nav-link" href="/Pregnancy">Pregnancy</a>
+          </b></li>
+          <li className="nav-item"><b>
+            <a className="nav-link" href="/">Sexual Education</a>
+          </b></li>
+          <li className="nav-item"><b>
+            <a className="nav-link" href="/">Youth Events</a>
+          </b></li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
