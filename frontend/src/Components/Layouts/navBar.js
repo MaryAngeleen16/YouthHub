14ca3cd6b79911683c9e264b575ea778a51b5514
armangracia <graciaarmansteven@gmail.com>
2024-02-24 17:23:30 +0800

    navbar update

--- frontend/src/Components/Layouts/navBar.js
+++ frontend/src/Components/Layouts/navBar.js
@@ -4,28 +4,47 @@ import './FH.css';
 const Navbar = () => {
   return (
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
-          <li className="nav-item color-nav"><b>
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item color-nav">
+          <b>
             <a className="nav-link header-link" href="/">Home</a>
-          </b></li>
-          <li className="nav-item color-nav"><b>
-            <a className="nav-link header-link" href="/postspage">All Posts</a>
-          </b></li>
-          <li className="nav-item color-nav"><b>
-            <a className="nav-link  header-link" href="/pregnancy">Pregnancy</a>
-          </b></li>
-          <li className="nav-item color-nav"><b>
+          </b>
+        </li>
+        <li className="nav-item color-nav dropdown">
+          <b>
+            <a className="nav-link header-link" href="/">Feed</a>
+          </b>
+          <ul className="dropdown-menu">
+            <li className="dropdown-item"><a href="#">All Post</a></li>
+            <li className="dropdown-item"><a href="/pregnancy">Pregnancy</a></li>
+            <li className="dropdown-item"><a href="#">Sexual Education</a></li>
+            <li className="dropdown-item"><a href="#">Contraceptive</a></li>
+            {/* Add more dropdown items as needed */}
+          </ul>
+        </li>
+        
+        <li className="nav-item color-nav">
+          <b>
+            <a className="nav-link header-link" href="/">Videos</a>
+          </b>
+        </li>
+        <li className="nav-item color-nav">
+          <b>
             <a className="nav-link header-link" href="/">Sexual Education</a>
-          </b></li>
-          <li className="nav-item color-nav"><b>
+          </b>
+        </li>
+        <li className="nav-item color-nav">
+          <b>
             <a className="nav-link header-link" href="/">Youth Events</a>
-          </b></li>
-          <li className="nav-item color-nav"><b>
+          </b>
+        </li>
+        <li className="nav-item color-nav">
+          <b>
             <a className="nav-link header-link" href="/forums">Forums</a>
-          </b></li>
-          
-        </ul>
-      </div>
+          </b>
+        </li>
+      </ul>
+    </div>
   );
 };
 
