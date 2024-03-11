009b94154be477e2dc7ca62b01381b4dc9726dd5
armangracia <graciaarmansteven@gmail.com>
2024-03-11 11:04:13 +0800

    landing page update

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -48,7 +48,7 @@ const Home = () => {
                 <div className="row-banner">
                     <div className="col-banner">
                         <div className="WomanReadingWithBaby">
-                            <img className="WomanReadingWithBaby" src="/images/girlbanner.png" alt="Banner" />
+                            <img className="WomanReadingWithBaby" src="/images/heartwoman.png" alt="Banner" />
                         </div>
                     </div>
 
@@ -133,7 +133,7 @@ const Home = () => {
                         <div class="AlthoughThisIsWel">IPDC Finance Limited (previously known as "Industrial Promotion and Development Company of Bangladesh Limited") is the first private sector financial institution of the country established in 1981 by a distinguished group of shareholders namely International Finance Corporation (IFC), USA, German Investment and Development Company (DEG), Germany, The Aga Khan Fund for Economic Development (AKFED), Switzerland, Commonwealth Development Corporation (CDC), UK and the Government of Bangladesh</div>
                     </div>
                     <div class="Frame25">
-                        <div class="ReadMore">Read More</div>
+                        <div class="ReadMore">Read Post</div>
                         <div class="RightArrow">
                             <div class="Vector"></div>
                         </div>
