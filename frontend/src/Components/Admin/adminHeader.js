40d697695258dd6dc0b6ca16d0025091af9b6678
armangracia <graciaarmansteven@gmail.com>
2024-03-13 00:14:25 +0800

    minor fix

--- frontend/src/Components/Admin/adminHeader.js
+++ frontend/src/Components/Admin/adminHeader.js
@@ -12,12 +12,12 @@ function Header({OpenSidebar}) {
             <BsJustify className='icon' onClick={OpenSidebar}/>
         </div>
         <div className='header-left'>
-            <BsSearch  className='icon'/>
+            {/* <BsSearch  className='icon'/> */}
         </div>
         <div className='header-right'>
-            <BsFillBellFill className='icon'/>
+            {/* <BsFillBellFill className='icon'/>
             <BsFillEnvelopeFill className='icon'/>
-            <BsPersonCircle className='icon'/>
+            <BsPersonCircle className='icon'/> */}
         </div>
     </header>
   )
